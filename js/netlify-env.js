9803075539420e860e11e5671352da6641c8104f
Michael Preciado <mpreciado1997@gmail.com>
2025-06-02 21:30:25 -0700

    chore: remove unused dev dependency for netlify-plugin-inline-env and clean up package configuration

---
+++ js/netlify-env.js
@@ -0,0 +1,10 @@
+
+// Environment variables injected by Netlify build process
+// This file is auto-generated - do not edit manually
+
+window.NETLIFY_ENV = {};
+
+// Also set individual globals for compatibility
+
+
+console.log('🚀 Netlify environment variables loaded:', Object.keys(window.NETLIFY_ENV || {}));
